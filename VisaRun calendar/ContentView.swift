f9a4156a1187883dc6c655c401aeb7cb22e9ebf1
bablodable <bablodable@gmail.com>
2023-11-26 19:32:45 +0100

    begin

---
+++ VisaRun calendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisaRun calendar
+//
+//  Created by Eduard Gareev on 11/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisaRun calendar/VisaRun_calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisaRun_calendarApp.swift
+//  VisaRun calendar
+//
+//  Created by Eduard Gareev on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct VisaRun_calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
